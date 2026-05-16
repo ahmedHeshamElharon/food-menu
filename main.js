71d3356fb88af64eb4ceca12108e31a336d661b6
MYoussefSoliman88 <mohammad.youssef@tamm.com>
2026-05-16 23:49:55 +0300

    more on functions

--- main.js
+++ main.js
@@ -46,26 +46,72 @@ function checkDrivingLicense(age, country) {
   var acceptLicense = "Can issue driving license";
   var notAcceptLicense = "Cannot issue driving license";
 
-  function logLicense() {
-    console.log(acceptLicense + " at " + country);
+  function logLicense(isAccepted) {
+    // closure function to log the license acceptance message
+    if (isAccepted) {
+      console.log(acceptLicense + " at " + country);
+    } else {
+      console.log(notAcceptLicense + " at " + country);
+    }
   }
 
-  if (!age || !country) {
-    console.log("age or country missing");
+  if (!age) {
+    console.log("age is missing");
+    return;
+  }
+  if (!country) {
+    console.log("Country is missing");
     return;
   }
 
   if ((country == "uae" || country == "ksa") && age >= 21) {
-    logLicense();
+    logLicense(true);
   }
   if (country == "egypt" && age >= 16) {
-    logLicense();
+    logLicense(true);
   } else if (country == "usa" && age >= 18) {
     // console.log(acceptLicense + " at " + country);
-    logLicense();
+    logLicense(true);
   } else {
-    console.log(notAcceptLicense + " at " + country);
+    logLicense(false);
   }
 }
 
-checkDrivingLicense();
+const changeTemperatureType = function (degree, type) {
+  let result = {};
+  if (!degree || isNaN(degree)) {
+    console.error("Please provide a valid degree");
+    return;
+  }
+  if (type != "celsius" && type != "fahrenheit") {
+    console.error(
+      "Invalid temperature type provided, please provide either celsius or fahrenheit",
+    );
+    return;
+  }
+
+  if (type === "fahrenheit") {
+    result.degree = (degree * 9) / 5 + 32;
+    result.type = "fahrenheit";
+  } else {
+    result.degree = ((degree - 32) * 5) / 9;
+    result.type = "celsius";
+  }
+  return result;
+};
+
+// update the dom
+const tempValue = document.getElementById("tem-output-value");
+const tempType = document.getElementById("tem-output-type");
+const example = changeTemperatureType(-30, "celsius");
+tempValue.innerText = example.degree;
+tempType.innerText = example.type;
+console.log(example);
+
+const student = {
+  name: "Ahmed Hesham",
+  age: 28,
+  section: "Engineering",
+  grade: 4,
+  degree: "Very good",
+};
